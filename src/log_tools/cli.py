c443a6d6599acdaadf1ea43ce4761a5e219ccb53
Matthew Westphall <westphall@wisc.edu>
2025-03-06 17:11:27 -0600

    Add basic scaffolding for sequence check commands

--- src/log_tools/cli.py
+++ src/log_tools/cli.py
@@ -2,11 +2,13 @@ import typer
 
 from .log_tools import filter_logs_by_date
 from .common_args import common_args
+from .sequence_check import sequence
 
 
 app = typer.Typer()
 app.callback()(common_args)
 app.command("filter")(filter_logs_by_date)
+app.add_typer(sequence, name="sequence")
 
 
 
---
+++ src/log_tools/sequence_check.py
@@ -0,0 +1,29 @@
+import typer
+from typing import Annotated
+from pathlib import Path
+from datetime import datetime
+from .common_args import common_args, LogPathOpt
+
+sequence = typer.Typer(help="Sub-commands to validate log sequence numbers")
+
+@sequence.command("list")
+def list_sequences(
+    ctx: typer.Context,
+    log_path: LogPathOpt,
+):
+    """ Given a set of log files - return the unique logger IDs appearing 
+    in those files 
+    """
+    pass
+
+
+
+@sequence.command("check")
+def check_sequence(
+    ctx: typer.Context,
+    log_path: LogPathOpt,
+):
+    """ Given a sequence ID appearing in a set of log files 
+    - return any gaps in that logger's sequence
+    """
+    pass
